f23b1913afb16186ed6926487443e6c645d2d2dc
PoLina <Polinochka_-95@mail.ru>
2022-02-01 23:02:27 +0300

    Kvadrat ili net

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа");
+string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+int num1 = int.Parse(s1);
+int num2 = int.Parse(s2);
+if (num1 == num2*num2)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
