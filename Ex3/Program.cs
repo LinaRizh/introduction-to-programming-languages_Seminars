38a94d6b308bd8d6630c5c7b056ba401ca46077b
PoLina <Polinochka_-95@mail.ru>
2022-02-01 23:28:53 +0300

    Max&Min

---
+++ Ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите два числа");
+string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+int num1 = int.Parse(s1);
+int num2 = int.Parse(s2);
+if (num2 == num1)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{ 
+    if (num1 > num2)
+    {
+        Console.Write("max число = ");
+        Console.WriteLine(num1);
+        Console.Write("min число = ");
+        Console.WriteLine(num2);
+    }
+
+    else
+    {
+        Console.Write("max число = ");
+        Console.WriteLine(num2);
+        Console.Write("min число = ");
+        Console.WriteLine(num1);
+    }
+}
