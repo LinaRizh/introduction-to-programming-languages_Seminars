661170aa5f8981ba79a40544db4b6c73fd5dba33
PoLina <Polinochka_-95@mail.ru>
2022-02-02 01:12:31 +0300

    Max from three numbers

---
+++ Ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите три числа");
+string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+string s3 = Console.ReadLine();
+int num1 = int.Parse(s1);
+int num2 = int.Parse(s2);
+int num3 = int.Parse(s3);
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+    if (num3 > max)
+    {
+        max = num3;
+    }
+}
+else
+{
+    if (num3 > max)
+    {
+        max = num3;
+    }
+}
+if (num1 == num2)
+{
+    if (num2 == num3)
+    {
+        Console.WriteLine("Числа равны");
+    }
+}
+else
+{
+    Console.Write("max число = ");
+    Console.WriteLine(max);
+}
