fa5cc67ba6301c2f802a788f961a64952e31b426
PoLina <Polinochka_-95@mail.ru>
2022-02-02 01:52:29 +0300

    Even numbers from 1 to N

---
+++ Ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+string s1 = Console.ReadLine();
+int N = int.Parse(s1);
+int i = 1;
+while (i < N)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i );
+    }
+    else
+    {
+        Console.Write(" ");
+    }
+    i ++;
+}
+Console.WriteLine();
+
+
