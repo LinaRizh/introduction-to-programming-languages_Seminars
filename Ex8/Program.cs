c420b03f748beebc6f741efe78f93fefc693c051
PoLina <Polinochka_-95@mail.ru>
2022-02-02 02:29:26 +0300

    Numbers from -N to N

---
+++ Ex8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string s1 = Console.ReadLine();
+int N = int.Parse(s1);
+int i = -N;
+while (i <= N)
+{
+        Console.Write(i );
+        Console.Write(" ");
+    i ++;
+}
+Console.WriteLine();
