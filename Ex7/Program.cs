3aa03db02d5fb1da121ada0843ef79630ba07b8c
PoLina <Polinochka_-95@mail.ru>
2022-02-02 01:32:51 +0300

    Even or odd number

---
+++ Ex7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string s1 = Console.ReadLine();
+int num1 = int.Parse(s1);
+if (num1%2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число является нечётным");
+}
