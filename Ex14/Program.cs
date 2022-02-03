7bd2adeafe6a37c7f298ec4546cba9549901f363
PoLina <Polinochka_-95@mail.ru>
2022-02-03 22:32:20 +0300

    Chislo cratno

---
+++ Ex14/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+string GetAnswer(int a, int b)
+{
+    if(a%b==0)
+    return $"Число {a} кратно {b}";
+    return (a%b).ToString();
+}
+
+int a1=int.Parse(ReadLine());
+int a2=int.Parse(ReadLine());
+
+WriteLine(GetAnswer(a1,a2));
